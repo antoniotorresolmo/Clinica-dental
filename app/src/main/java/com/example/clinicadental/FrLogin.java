13ea9e76fa3ed741a936694656316709d0deb4b5
juanmtena <juanmtena@hotmail.com>
2022-01-11 14:33:33 +0100

    subo pero con errores

---
+++ app/src/main/java/com/example/clinicadental/FrLogin.java
@@ -0,0 +1,26 @@
+package com.example.clinicadental;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FrLogin extends Fragment {
+
+    public FrLogin() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fr_login, container, false);
+    }
+}
--- app/src/main/java/com/example/clinicadental/MainActivity.java
+++ app/src/main/java/com/example/clinicadental/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.clinicadental;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FrLogin frLogin = new FrLogin();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.frgContainer, frLogin, null);
+        ft.commit();
+
     }
 }
