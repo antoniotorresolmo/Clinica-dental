f18ed187b2aef8265c0a6a08764dfc6968877d2e
dpriegoo <77279312+dpriegoo@users.noreply.github.com>
2022-01-11 09:00:36 +0100

    TODO

--- app/src/main/java/com/example/vistas_perfil/MainActivity.java
+++ app/src/main/java/com/example/vistas_perfil/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         FrPerfil frPerfil = new FrPerfil();
         FragmentManager oFragmentManger = getSupportFragmentManager();
