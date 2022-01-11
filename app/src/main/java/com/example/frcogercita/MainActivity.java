d3927e033823df64377d34c7fb17d497489d8a23
juanmtena <juanmtena@hotmail.com>
2022-01-11 14:08:38 +0100

    subo bien el fragment de coger cita

--- app/src/main/java/com/example/frcogercita/MainActivity.java
+++ app/src/main/java/com/example/frcogercita/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.frcogercita;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -11,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FrCogerCita frCogerCita = new FrCogerCita();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.frgContainer, frCogerCita, null);
+        ft.commit();
 
     }
 }
