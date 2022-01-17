3e23ff7baa43a73cc66d099e2107cdf69541cf26
juanmtena <juanmtena@hotmail.com>
2022-01-17 11:59:27 +0100

    cambio el registro de fragment a activity y al pulsar el boton Registro que se abra la activity

--- Clinica-dental/app/src/main/java/com/example/clinicadental/MainActivity.java
+++ Clinica-dental/app/src/main/java/com/example/clinicadental/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.clinicadental;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        findViewById(R.id.btnRegistrar).setOnClickListener(v-> {
+            Intent ventana = new Intent(MainActivity.this, Registro.class);
+            startActivity(ventana);
+        });
+
     }
 }
