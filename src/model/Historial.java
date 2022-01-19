5c352c6fdbb5bf74bd9ace2f52f2d04affdad589
Juan <juanmtena@hotmail.com>
2022-01-19 10:50:43 +0100

    hago cambios en el modelo Historial

--- src/model/Historial.java
+++ src/model/Historial.java
@@ -2,60 +2,64 @@ package model;
 
 public class Historial {
 	
-	private int id_Consulta; //PK
-	private String operacion; //NN
-	private String observacines; //NN
-	private Paciente id_Paciente; //FK
+	private int ID_Consulta; //PK
+	private String Operacion; //NN
+	private String Observacines; //NN
+	private Paciente ID_Paciente; //FK
 	
-	public Historial(int id_Consulta) {
+	public Historial(int ID_Consulta) {
 		
-		this.id_Consulta = id_Consulta;
+		this.ID_Consulta = ID_Consulta;
 	}
 
-	public Historial(int id_Consulta, String operacion, String observacines, Paciente id_Paciente) {
+	public Historial(int ID_Consulta, String Operacion, String Observacines, Paciente ID_Paciente) {
 		super();
-		this.id_Consulta = id_Consulta;
-		this.operacion = operacion;
-		this.observacines = observacines;
-		this.id_Paciente = id_Paciente;
+		this.ID_Consulta = ID_Consulta;
+		this.Operacion = Operacion;
+		this.Observacines = Observacines;
+		this.ID_Paciente = ID_Paciente;
 	}
 
 	public int getId_Consulta() {
-		return id_Consulta;
+		return ID_Consulta;
 	}
 
-	public void setId_Consulta(int id_Consulta) {
-		this.id_Consulta = id_Consulta;
+	public void setId_Consulta(int ID_Consulta) {
+		this.ID_Consulta = ID_Consulta;
 	}
 
 	public String getOperacion() {
-		return operacion;
+		return Operacion;
 	}
 
-	public void setOperacion(String operacion) {
-		this.operacion = operacion;
+	public void setOperacion(String Operacion) {
+		if (Operacion != null && Operacion.length() <= 255) {
+			this.Operacion = Operacion;
+		}
 	}
 
 	public String getObservacines() {
-		return observacines;
+		return Observacines;
 	}
 
-	public void setObservacines(String observacines) {
-		this.observacines = observacines;
+	public void setObservacines(String Observacines) {
+		if (Observacines != null && Observacines.length() <= 1000) {
+			this.Observacines = Observacines;
+		}
 	}
 
 	public Paciente getId_Paciente() {
-		return id_Paciente;
+		return ID_Paciente;
 	}
 
-	public void setId_Paciente(Paciente id_Paciente) {
-		this.id_Paciente = id_Paciente;
+	public void setId_Paciente(Paciente ID_Paciente) {
+		this.ID_Paciente = ID_Paciente;
 	}
 
 	@Override
 	public String toString() {
-		return "Historial [id_Consulta=" + id_Consulta + ", operacion=" + operacion + ", observacines=" + observacines
-				+ ", id_Paciente=" + id_Paciente + "]";
+		return "Historial [id_Consulta=" + ID_Consulta + ", operacion=" + Operacion + ", observacines=" + Observacines
+				+ ", id_Paciente=" + ID_Paciente + "]";
 	}
 
 }
